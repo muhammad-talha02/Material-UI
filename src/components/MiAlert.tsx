7cda416b503874d12bf16fb373f0e1780b99e0ec
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2023-09-25 05:36:27 +0000

    Update -> ALert Component

--- src/components/MiAlert.tsx
+++ src/components/MiAlert.tsx
@@ -3,7 +3,7 @@ import { Check } from "@mui/icons-material";
 const MiAlert = () => {
   return (
     <Stack spacing={2}>
-        <h1>Alert component</h1>
+      <h1>Alert component</h1>
       <Alert severity="error">This a Error Message</Alert>
       <Alert severity="info">This a info Message</Alert>
       <Alert severity="warning">This a warning Message</Alert>
@@ -20,13 +20,19 @@ const MiAlert = () => {
       <Alert variant="outlined" severity="success">
         This a success Message
       </Alert>
-      <Alert variant="filled" severity="error" onClose={()=>alert("closing...")}>
+      <Alert
+        variant="filled"
+        severity="error"
+        onClose={() => alert("closing...")}
+      >
         <AlertTitle>Error</AlertTitle>
         This a Error Message
       </Alert>
-      <Alert variant="filled" severity="info" action={
-        <Button color="inherit">UNDO</Button>
-      }>
+      <Alert
+        variant="filled"
+        severity="info"
+        action={<Button color="inherit">UNDO</Button>}
+      >
         <AlertTitle>Info</AlertTitle>
         This a info Message
       </Alert>
@@ -34,7 +40,11 @@ const MiAlert = () => {
         <AlertTitle>Warning</AlertTitle>
         This a warning Message
       </Alert>
-      <Alert variant="filled" severity="success" icon={<Check fontSize="inherit"/>}>
+      <Alert
+        variant="filled"
+        severity="success"
+        icon={<Check fontSize="inherit" />}
+      >
         <AlertTitle>Success</AlertTitle>
         This a success Message
       </Alert>
